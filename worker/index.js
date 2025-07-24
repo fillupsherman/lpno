1894b5dce5c6317f5de3834bc784607d949ff237
DESKTOP\fillu <phillipsherman@phillipsherman.com>
2025-07-24 08:40:05 -0400

    i give up

--- worker/index.js
+++ worker/index.js
@@ -31,7 +31,7 @@ export default {
             groupByUrlname(urlname: $slug) {
               events(
                 first: 20,
-                filter: { status: FUTURE }
+                filter: { status: "FUTURE" }
               ) {
                 edges {
                   node {
